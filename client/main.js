cbd5dd14d9222e9e834a185d64283637e5577424
Spinaldash <blueavenger@gmail.com>
2015-02-09 15:52:18 -0800

    incomplete - creating a summation of the li odds and li evens elements

---
+++ client/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#add').click(add);
+}
+function add() {
+  var sum = $('#nums').val().split(',')
+  .map(function(letter) {
+    var num =  parseInt(letter);
+    var $li =  $('<li>');
+    $li.text(num);
+
+    if (num % 2) {
+      //odds
+      $('#odds').append($li);
+    } else {
+      //evens
+      $('#evens').append($li);
+    }
+
+    return num;
+  }).reduce(function(prev, curr) {
+    return prev + curr;
+  }, 0);
+
+  $('#sum').text(sum);
+
+  var oddsum = total('#odds li');
+  var evensum = total('#evens li');
+
+  $('#oddsum').text('oddsum');
+  $('#evensum').text('evensum');
+
+
+  // var oddsum = $('#odds li')
+  // .map(function(index, element) {
+  //  return parseInt(element.textContent);
+  // });
+
+}
